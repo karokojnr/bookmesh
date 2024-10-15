618f0a416a9617729859ab264337ae568326e93e
karoko <karoko.jnr99@gmail.com>
2024-10-15 18:31:58 +0300

    feat: scaffolded the order service

--- orders/main.go
+++ orders/main.go
@@ -1,5 +1,10 @@
 package main
 
 func main() {
-	
+
+	store := NewStorage()
+	svc := NewService(store)
+
+	svc.CreateOrder()
+
 }
--- orders/storage.go
+++ orders/storage.go
@@ -1,5 +1,7 @@
 package main
 
+import "context"
+
 type storage struct {
 	// mongo db
 }
@@ -8,6 +10,6 @@ func NewStorage() *storage {
 	return &storage{}
 }
 
-func (s *storage) Create() error {
+func (s *storage) Create(ctx context.Context) error {
 	return nil
 }
